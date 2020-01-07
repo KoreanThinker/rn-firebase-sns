4ea31fec20822063d760c0f30b6293837a46930c
hyun <coderhyun476@gmail.com>
2020-01-07 23:54:16 +0900

    fast-image 연동

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View, Text, TouchableWithoutFeedback } from 'react-native'
 import useNavigation from '../../hooks/useNavigation';
+import FastImage from 'react-native-fast-image'
 
 const HomeScreen = () => {
     const navigatoin = useNavigation();
@@ -9,6 +10,13 @@ const HomeScreen = () => {
             <TouchableWithoutFeedback onPress={() => navigatoin.navigate('PostScreen')}>
                 <Text>Post</Text>
             </TouchableWithoutFeedback>
+            <FastImage
+                style={{ width: 200, height: 200 }}
+                source={{
+                    uri: 'https://github.com/DylanVann/react-native-fast-image/raw/master/docs/assets/scroll.gif',
+                }}
+                resizeMode={FastImage.resizeMode.contain}
+            />
         </View>
     )
 }
